acba12626bbba23eec10c68e98b40c16f0e5fbf5
DanielCorreaVilla_Examen_Git <dcorreavilla3@gmail.com>
2024-05-07 11:17:29 +0200

    HTML y CSS modificado. Header y Quienes somos modificado

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+// Añadimos el listener para detectar el scroll del usuario
+window.addEventListener( 'scroll', function() {
+    
+    // Capturamos los elementos a modificar
+    let header = document.getElementById( 'header' );
+    let ul     = document.getElementById( 'ul_navbar' );
+
+    // Definimos la altura del usuario máximo antes del cambio de color
+    let max_cap = 500;
+
+    // Si el usuario pasa de la altura máxima, cambiamos de color
+    if( window.scrollY > max_cap ) {
+        header.classList.remove( 'bg_transparent' );
+        ul.classList.remove( 'col_white' );
+        
+        header.classList.add( 'bg_white' );
+        ul.classList.add( 'col_black' );
+    } else {
+        header.classList.remove( 'bg_white' );
+        ul.classList.remove( 'col_black' );
+    }
+} );
