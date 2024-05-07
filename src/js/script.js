86ef73cceacabc81d28bde636945c962d0fb446a
DanielCorreaVilla_Examen_Git <dcorreavilla3@gmail.com>
2024-05-07 14:09:10 +0200

    Flecha de navegaión añadida

--- src/js/script.js
+++ src/js/script.js
@@ -4,9 +4,15 @@ window.addEventListener( 'scroll', function() {
     // Capturamos los elementos a modificar
     let header = document.getElementById( 'header' );
     let ul = document.getElementById( 'ul_navbar' );
+    let floating_arrow = document.getElementById( 'floating_arrow' );
 
     // Definimos la altura del usuario máximo antes del cambio de color
-    let max_cap = 500;
+    let max_cap = 775;
+
+    if( this.window.scrollY > 0 )
+        floating_arrow.style.display = 'block';
+    else
+        floating_arrow.style.display = 'none';
 
     // Si el usuario pasa de la altura máxima, cambiamos de color
     if (window.scrollY > max_cap) {
