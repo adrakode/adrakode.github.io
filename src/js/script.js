f3b8d1d56492484ec4415621996492c137939e38
Adri Arcones <adrianarconesgomez@gmail.com>
2024-05-07 13:47:45 +0200

    cambios fuentes texto

--- src/js/script.js
+++ src/js/script.js
@@ -3,20 +3,17 @@ window.addEventListener( 'scroll', function() {
     
     // Capturamos los elementos a modificar
     let header = document.getElementById( 'header' );
-    let ul     = document.getElementById( 'ul_navbar' );
+    let ul = document.getElementById( 'ul_navbar' );
 
     // Definimos la altura del usuario máximo antes del cambio de color
     let max_cap = 500;
 
     // Si el usuario pasa de la altura máxima, cambiamos de color
-    if( window.scrollY > max_cap ) {
-        header.classList.remove( 'bg_transparent' );
-        ul.classList.remove( 'col_white' );
-        
-        header.classList.add( 'bg_white' );
-        ul.classList.add( 'col_black' );
+    if (window.scrollY > max_cap) {
+        header.classList.add('bg_white');
+        ul.classList.add('text_black');
     } else {
-        header.classList.remove( 'bg_white' );
-        ul.classList.remove( 'col_black' );
+        header.classList.remove('bg_white');
+        ul.classList.remove('text_black');
     }
 } );
