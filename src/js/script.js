e862232db104c2667b9c7cefa1fe24ad368eca70
Adri Arcones <adrianarconesgomez@gmail.com>
2024-05-14 19:35:55 +0200

    Cambios en el acordeon y comentarios en el Script

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,27 @@
-// Añadimos el listener para detectar el scroll del usuario
-window.addEventListener( 'scroll', function() {
+///////////////////////////////////
+// FUNCIONALIDAD SCROLL HEADER
+///////////////////////////////////
+
+// Añadimos un listener para detectar el scroll del usuario
+window.addEventListener('scroll', function() {
     
     // Capturamos los elementos a modificar
-    let header = document.getElementById( 'header' );
-    let ul = document.getElementById( 'ul_navbar' );
-    let floating_arrow = document.getElementById( 'floating_arrow' );
-    let modButton = document.getElementById( 'nav_button' ); 
-    let logo = document.getElementById( 'logo' ); 
+    let header = document.getElementById('header');
+    let ul = document.getElementById('ul_navbar');
+    let floating_arrow = document.getElementById('floating_arrow');
+    let modButton = document.getElementById('nav_button'); 
+    let logo = document.getElementById('logo'); 
 
-    // Definimos la altura del usuario máximo antes del cambio de color
+    // Definimos la altura máxima del scroll antes del cambio de color
     let max_cap = 100;
 
-    if( this.window.scrollY > 500 )
+    // Si la posición de scroll supera cierto umbral, mostramos una flecha flotante
+    if (this.window.scrollY > 500)
         floating_arrow.style.display = 'block';
     else
         floating_arrow.style.display = 'none';
 
-    // Si el usuario pasa de la altura máxima, cambiamos de color
+    // Si el usuario ha hecho scroll más allá del límite, cambiamos los estilos
     if (window.scrollY > max_cap) {
         header.classList.add('bg_white');
         ul.classList.add('text_black');
@@ -30,38 +35,65 @@ window.addEventListener( 'scroll', function() {
     }
 });
 
+///////////////////////////////////
+// FUNCIONALIDAD DEL ACORDEON
+///////////////////////////////////
+
+// Al cargar la página, verifica si los ítems del acordeón están activos y ajusta las flechas si es necesario
+document.addEventListener("DOMContentLoaded", function() {
+    const accordionItems = document.querySelectorAll('.accordion-item');
+    accordionItems.forEach(item => {
+        toggleArrow(item);
+    });
+});
+
+// Al hacer clic en el acordeón, define el ítem como activo y muestra el contenido correspondiente
 const accordionItems = document.querySelectorAll('.accordion-item');
 
 accordionItems.forEach(item => {
     item.addEventListener('click', () => {
         item.classList.toggle('active');
-        const arrow = item.querySelector('.arrow');
-        arrow.classList.toggle('active');
+        toggleArrow(item);
     });
 });
+
+// Función para cambiar la rotación de la flecha según el estado del ítem del acordeón
+function toggleArrow(item) {
+    const arrow = item.querySelector('.arrow');
+    arrow.style.transform = item.classList.contains('active') ? 'rotate(180deg)' : 'rotate(0deg)';
+}
+
+
+
+///////////////////////////////////
+// FUNCIONALIDAD BOTON ALTERNAR
+///////////////////////////////////
+
 document.addEventListener("DOMContentLoaded", function() {
     var toggleBtn = document.getElementById("toggle-btn");
     var header = document.getElementById("header");
-    var div_black = document.getElementById( 'black' );
+    var div_black = document.getElementById('black');
     const toggleButton = document.getElementById('toggle-btn');
     const navBar = document.querySelector('nav');
 
     toggleBtn.addEventListener("click", function() {
+        // Toggle para mostrar/ocultar el encabezado y la capa negra
         if (header.style.display === "none") {
             header.style.display = "block";
             div_black.style.display = 'block';
             
-            document.body.classList.add( 'overflow-hidden' );
+            document.body.classList.add('overflow-hidden'); // Evita el scroll cuando el menú está abierto
         } else {
             header.style.display = "none";
             div_black.style.display = 'none';
-            document.body.classList.remove( 'overflow-hidden' );
+            document.body.classList.remove('overflow-hidden');
         }
+
         toggleButton.addEventListener('click', function() {
-            navBar.classList.toggle('show');
+            navBar.classList.toggle('show'); // Agrega/elimina una clase para mostrar/ocultar el menú
         });
     
-        // Cerrar el menú al hacer clic en un enlace
+        // Cierra el menú al hacer clic en un enlace
         const navLinks = document.querySelectorAll('nav ul li a');
         navLinks.forEach(link => {
             link.addEventListener('click', function() {
@@ -69,16 +101,9 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         });
     });
-    
 });
-// document.getElementById("close-btn").addEventListener("click", function() {
-//     // Ocultar el menú, puedes hacerlo cambiando clases o directamente modificando el estilo
-//     document.getElementById("header").classList.remove("show-menu");
-// });
 
-
+// Listener para recargar la página cuando cambia el tamaño de la ventana
 window.addEventListener('resize', function() {
-    // Recargar la página
     window.location.reload();
 });
-
